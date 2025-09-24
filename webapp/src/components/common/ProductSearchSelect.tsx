3153214082f48807f4831f6d2fc4e48336d30a75
atiti436 <atiti4361@gmail.com>
2025-09-25 00:05:08 +0800

    fix(ui): ProductSearchSelect parser should return number instead of string

--- webapp/src/components/common/ProductSearchSelect.tsx
+++ webapp/src/components/common/ProductSearchSelect.tsx
@@ -407,7 +407,11 @@ export default function ProductSearchSelect({
               min={0}
               style={{ width: '100%' }}
               formatter={value => `$ ${value}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-              parser={value => value!.replace(/\$\s?|(,*)/g, '')}
+              parser={value => {
+                const cleaned = (value || '').replace(/\$\s?|,/g, '')
+                const num = parseFloat(cleaned)
+                return isNaN(num) ? 0 : num
+              }}
             />
           </Form.Item>
 
